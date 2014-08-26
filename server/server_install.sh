d99e4e71c76b77cf47210b4d311ca2f632b0f952
Florian Eckerstorfer <florian@theroadtojoy.at>
2014-08-26 22:33:51 +0200

    Fixed installation script for Nginx

--- server/server_install.sh
+++ server/server_install.sh
@@ -14,14 +14,14 @@ gem --version
 
 # Install Nginx
 if [[ ! -f /usr/local/nginx/sbin/nginx ]]; then
-    wget http://nginx.org/download/nginx-1.7.0.tar.gz
-    tar -xvpzf nginx-1.7.0.tar.gz
-    cd nginx-1.7.0.tar.gz
-    ./configure --with-http_spdy_module --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-mail --with-mail_ssl_module --with-file-aio --with-ipv6 --with-cc-opt='-O2 -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic'
+    wget http://nginx.org/download/nginx-1.7.3.tar.gz
+    tar -xvpzf nginx-1.7.3.tar.gz
+    cd nginx-1.7.3
+    ./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-http_spdy_module --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-mail --with-mail_ssl_module --with-file-aio --with-ipv6 --with-cc-opt='-O2 -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic'
     make
     make install
     cd ..
-    rm -rf nginx-1.7.0.tar.gz nginx-1.7.0
+    rm -rf nginx-1.7.3.tar.gz nginx-1.7.3
 fi
 
 if [[ ! -f /etc/init.d/nginx ]]; then
