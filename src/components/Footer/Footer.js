88324ec5b8adfd997668d5141b0c4a9a439f131d
Florian Eckerstorfer <florian@eckerstorfer.net>
2018-06-06 15:10:01 +0200

    Add CV

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,13 @@ import Link from 'gatsby-link';
 import './footer.scss';
 
 const Footer = () => (
-  <footer className="footer">
-    Made by Florian Eckerstorfer in Vienna, Europe.
-    <Link to="/imprint">Imprint</Link>
-    <Link to="/privacy">Privacy Policy</Link>
+  <footer className="footer h-card">
+    Made by{' '}
+    <a href="https://florian.ec" className="p-name u-url">
+      Florian Eckerstorfer
+    </a>{' '}
+    in <span className="p-locality">Vienna</span>, Europe.{' '}
+    <Link to="/imprint">Imprint</Link> <Link to="/privacy">Privacy Policy</Link>
   </footer>
 );
 
