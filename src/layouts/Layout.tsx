eb0d80943fc97d298819e6e878dd4841134de5dd
Florian Eckerstorfer <florian@eckerstorfer.net>
2019-04-12 19:00:24 +0200

    main nav on right, style code blocks

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import 'prism-theme-night-owl';
 import { StaticQuery, graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 import Header from '../components/Header/Header';
 import './layout.css';
