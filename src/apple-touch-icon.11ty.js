28ba39575af3017a0b476b29199af5390cb0689f
Florian Eckerstorfer <florian@eckerstorfer.net>
2019-11-16 00:29:00 +0100

    feat(AppleTouchIcon): Generate apple-touch-icons on build

---
+++ src/apple-touch-icon.11ty.js
@@ -0,0 +1,31 @@
+const sharp = require('sharp');
+const promisify = require('util').promisify;
+const fs = require('fs');
+
+const readFile = promisify(fs.readFile);
+
+class AppleTouchIcon {
+  async readOriginalIcon() {
+    return await readFile('./src/images/apple-touch-icon.png');
+  }
+
+  async resizeIcon(icon, size) {
+    return await sharp(icon)
+      .resize(size, size)
+      .toFile(`./dist/images/apple-touch-icon-${size}x${size}.png`);
+  }
+
+  async render() {
+    const iconSizes = [120, 152, 167, 180];
+    const originalIcon = await this.readOriginalIcon();
+    await Promise.all(
+      iconSizes.map(async size => {
+        this.resizeIcon(originalIcon, size);
+      })
+    );
+
+    return false;
+  }
+}
+
+module.exports = AppleTouchIcon;
