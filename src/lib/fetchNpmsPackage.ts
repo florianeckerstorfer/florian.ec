b6dd8940af81c5d95bd166982181f50f7ab695b0
Florian Eckerstorfer <florian@eckerstorfer.net>
2020-01-29 21:33:04 +0100

    chore: Remove files from Gatsby

--- src/lib/fetchNpmsPackage.ts
+++
@@ -1,8 +0,0 @@
-import fetch from 'cross-fetch';
-
-async function fetchNpmsPackage(packageName: string): Promise<NPMSPackage> {
-  const response = await fetch(`https://api.npms.io/v2/package/${packageName}`);
-  return await response.json();
-}
-
-export default fetchNpmsPackage;
