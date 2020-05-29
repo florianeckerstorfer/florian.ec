685d2dd498008e2ad502a71d27287a0081d666ce
Florian Eckerstorfer <florian.eckerstorfer@mysugr.com>
2020-05-29 22:54:13 +0200

    Add "gatsby-remark-a11y-emoji" to projects list

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,19 @@ function build() {
     .pipe($.rename({ suffix: '-320' }))
     .pipe($.newer('dist'))
     .pipe(
-      $.rename(options => {
+      $.rename((options) => {
         options.basename = options.basename.replace('-320', '');
         return options;
       })
     )
     .pipe(
-      $.responsive(
-        {
-          '**/*.{jpg,jpeg,png}': [
-            { width: 1920, rename: { suffix: '-1920' } },
-            { width: 1280, rename: { suffix: '-1280' } },
-            { width: 960, rename: { suffix: '-960' } },
-            { width: 640, rename: { suffix: '-640' } },
-            { width: 320, rename: { suffix: '-320' } },
-          ],
-        },
-        {
-          errorOnEnlargement: false,
-          withMetadata: false,
-          skipOnEnlargement: true,
-          errorOnUnusedConfig: false,
-          errorOnUnusedImage: false,
-        }
-      )
+      $.rezzy([
+        { width: 1920, suffix: '-1920' },
+        { width: 1280, suffix: '-1280' },
+        { width: 960, suffix: '-960' },
+        { width: 640, suffix: '-640' },
+        { width: 320, suffix: '-320' },
+      ])
     )
     .pipe(gulp.dest('dist'));
 }
---
+++ src/projects/gatsby-remark-a11y-emoji/index.md
@@ -0,0 +1,10 @@
+---
+slug: gatsby-remark-a11y-emoji
+title: "gatsby-remark-a11y-emoji"
+description: Gatsby Plugin to make Emoji in Markdown accessible. Wraps Emoji in a <span>-Tag with role and aria-labelledby attributes.
+date: 2018-05-04
+category: Gatsby Plugins
+tags: [gatsby, gatsby plugin, open source]
+active: true
+link: https://github.com/florianeckerstorfer/gatsby-remark-a11y-emoji
+---
