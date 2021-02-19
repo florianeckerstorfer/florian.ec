4c0e121ff6181c74cb76714b76d7dadbfddab4b2
Florian Eckerstorfer <florian@eckerstorfer.net>
2021-02-19 18:11:06 +0100

    Remove region from CI awscli

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync dist/ s3://florian-ec-prod --profile florian.ec --region eu-west-1
-aws cloudfront create-invalidation --distribution-id E31F182UN8ILBI --paths '/*' --profile florian.ec --region eu-west-1
+aws s3 sync dist/ s3://florian-ec-prod --profile florian.ec
+aws cloudfront create-invalidation --distribution-id E31F182UN8ILBI --paths '/*' --profile florian.ec
