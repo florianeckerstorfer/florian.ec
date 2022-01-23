16ea6f2242fc08685961f068c805519dbc14d123
Florian Eckerstorfer <florian@eckerstorfer.net>
2022-01-23 09:52:41 +0100

    chore: Deploy to new server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-aws s3 sync dist/ s3://florian-ec-prod --profile florian.ec --region eu-west-1
-aws cloudfront create-invalidation --distribution-id E31F182UN8ILBI --paths '/*' --profile florian.ec --region eu-west-1
+deploy="www@158.255.211.173"
+sshopts="ssh -o StrictHostKeyChecking=no"
+rsync --rsh="$sshopts" -rP dist/ $deploy:/var/www/florian.ec
